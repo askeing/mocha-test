8c5f5297eedff9e6e4d8241de97443020dc29099
askeing <askeing@gmail.com>
2015-09-14 16:02:08 +0800

    Update the test code

--- test/first_test.js
+++ test/first_test.js
@@ -1,6 +1,14 @@
 var assert = require("assert");
 var first_module = require('../lib/first_module');
 
+// first test
+var foo = 'bar';
+describe('First', function () {
+  it('Test', function () {
+    assert.equal(foo, 'bar');
+  });
+});
+
 describe('Array', function() {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
